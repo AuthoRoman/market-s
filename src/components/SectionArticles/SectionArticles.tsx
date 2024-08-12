592f840862c3aca3545de27ea2e7ca2cdac9a4c2
AuthoRoman <kit.kiram2017@yandex.ru>
2024-08-12 23:49:48 +0300

    fix: fix scope mobile footer and articles

--- src/components/SectionArticles/SectionArticles.tsx
+++ src/components/SectionArticles/SectionArticles.tsx
@@ -4,9 +4,9 @@ import ArticleCard from "./ArticleCard";
 
 const SectionArticles = () => {
   return (
-    <div className="max-w-[1208px] m-auto flex  flex-col  my-[120px] max-[1208px]:max-w-[736px]">
+    <div className="max-w-[1208px] m-auto flex  flex-col  my-[120px] max-[1208px]:max-w-[736px] max-[900px]:max-w-[336px] ">
       <HeaderSection limited={true}  title="articles" />
-      <div className="flex wrap m-w-[1208px] items-center gap-10  max-[1208px]:max-w-[736px]">
+      <div className="flex flex-wrap max-w-[1208px] items-center gap-10  max-[1208px]:max-w-[736px]   max-[900px]:items-center">
         <div>
           <ArticleCard type="stop_mask" />
         </div>
